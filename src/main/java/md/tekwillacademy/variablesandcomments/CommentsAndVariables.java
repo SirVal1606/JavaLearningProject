10208ef94e747cc0d0599614173c497b44266bfd
Valeria <valeria.sirbu2000@gmail.com>
2024-04-15 17:47:05 +0300

    added 2 new variables for each type and initialized them.

---
+++ src/main/java/md/tekwillacademy/variablesandcomments/CommentsAndVariables.java
@@ -0,0 +1,67 @@
+package md.tekwillacademy.variablesandcomments;
+
+import jdk.jshell.execution.Util;
+
+import java.lang.reflect.Array;
+
+public class CommentsAndVariables {
+    public static void main(String[] args) {
+        boolean variabilaBoolean1;
+        variabilaBoolean1=true;
+        System.out.println("the value for Boolean: "+variabilaBoolean1);
+        boolean variabilaBoolean2=false;
+        System.out.println("the value for Boolean2: "+variabilaBoolean2);
+
+        char variabilaChar1 = 'z';
+        System.out.println("the value for Char: "+variabilaChar1);
+        char variabilaChar2 = '9';
+        System.out.println("the value for Char: "+variabilaChar2);
+
+        byte variabilaByte1=127;
+        System.out.println("the value for Byte: "+variabilaByte1);
+        byte variabilaByte2= -127;
+        System.out.println("the value for Byte: "+variabilaByte2);
+
+        short variabilaShort1=32767;
+        System.out.println("the value for Short: "+variabilaShort1);
+        short variabilaShort2=-32767;
+        System.out.println("the value for Short: "+variabilaShort2);
+
+        int variabilaInt1=2000000;
+        System.out.println("the value for Int: "+variabilaInt1);
+        int variabilaInt2=3000000;
+        System.out.println("the value for Int: "+variabilaInt2);
+
+        long variabilaLong1=5999999;
+        System.out.println("the value for Long: "+variabilaLong1);
+        long variabilaLong2=-5999999;
+        System.out.println("the value for Long: "+variabilaLong2);
+
+        float variabilaFloat1=14.5555f;
+        System.out.println("the value for Float: "+variabilaFloat1);
+        float variabilaFloat2=14666666.5555f;
+        System.out.println("the value for Float: "+variabilaFloat2);
+
+        double variabilaDouble1=14.787878787878787878d;
+        System.out.println("the value for Double: "+variabilaDouble1);
+        double variabilaDouble2=1478787878787878787.8d;
+        System.out.println("the value for Double: "+variabilaDouble2);
+        // Variabile de referinta
+
+        String obiectString;
+        obiectString = new String();
+
+
+
+
+
+
+
+
+
+    }
+
+
+
+
+}

f3be43502c9d78b6c452b811b7e4892ec7eb0d7a
VSirbu <valeria.sirbu2000@gmail.com>
2024-04-24 12:41:52 +0300

    Added the Java Code for an Auto Service app.

---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/Garage.java
@@ -0,0 +1,19 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class Garage {
+    String address;
+    int numberOfSpots;
+    double dimensionSqFt;
+
+//definire constructor
+    public Garage(String addressUsedForInitialization, int numberOfSpots, double dimensionSqFt){
+        address = addressUsedForInitialization;
+        this.numberOfSpots = numberOfSpots;
+        this.dimensionSqFt = dimensionSqFt;
+    }
+    public Garage(){
+        System.out.println("A fost creat un obiect care nu are nici un paramentru populat cu date concrete " +
+                "acesta este un constructor definit fara paramentri");
+    }
+
+}
---
+++ src/main/java/md/tekwillacademy/classesandobjects/autoservicetask/ManageAutoService.java
@@ -0,0 +1,39 @@
+package md.tekwillacademy.classesandobjects.autoservicetask;
+
+public class ManageAutoService {
+    public static void main(String[] args) {
+
+        Garage masterLuxObject = new Garage("Str. Calea orheiului 147", 3, 220);
+        System.out.println(masterLuxObject.address);
+
+        Garage autoLux = new Garage();
+        autoLux.address = "Str. Stefan cel Mare 150";
+        autoLux.dimensionSqFt = 250.6;
+        autoLux.numberOfSpots = 5;
+        System.out.println(autoLux.address);
+        System.out.println(autoLux.numberOfSpots);
+        System.out.println(autoLux.dimensionSqFt);
+        System.out.println("Informatii despre garajul autoLux: adresa "+ autoLux.address +" cu numarul de spatii "
+                +autoLux.numberOfSpots +" si dimensiunea "+autoLux.dimensionSqFt);
+
+        Tool hammer01 = new Tool("Hammer");
+        System.out.println(hammer01.nameOfTheTool);
+
+        Tool key = new Tool("Lucky key");
+        System.out.println(key.nameOfTheTool);
+
+        Worker ionelCelHarnic = new Worker("Ion");
+        System.out.println(ionelCelHarnic.pricePerHour);
+        ionelCelHarnic.yearsOfExperience =  5;
+        System.out.println(ionelCelHarnic.yearsOfExperience);
+
+        Worker marcelCelPuternic = new Worker();
+
+        Car masinaNicoletei = new Car("Ferrari","Red",3,4500);
+        System.out.println(masinaNicoletei.marca);
+
+        Car masinaLuiAdrian = new Car("Mercedes","Black",3,5000);
+        System.out.println(masinaLuiAdrian.colorOfTheCar);
+
+    }
+}
